39ae6aec2d61c6b6963c2e5162b7768c0b6845fb
Rayldo Azevedo <azevedorayldo@gmail.com>
2022-04-18 13:14:05 -0300

    react-icons add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { Select, Icon, DatePicker } from 'react-materialize'
+import { Select, Icon, DatePicker, } from 'react-materialize'
+import { FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa'
+
 import 'materialize-css'
 import './App.css';
 
@@ -313,7 +315,7 @@ function App() {
       <footer className="page-footer blue-grey darken-4">
         <div className="container">
           <div className="row">
-            <div className="col m4 l3 s12">
+            <div className="col m4 l2 s12">
               <h5 className="white-text">CMP 1491</h5>
               <p className="grey-text text-lighten-4">Responsividade com Materialize</p>
             </div>
@@ -321,17 +323,29 @@ function App() {
               <h5 className="white-text">Desenvolvimento</h5>
               <p className="grey-text text-lighten-4">Rayldo Azevedo e Estevão Sampaio</p>
             </div>
-            <div className="col m4 l3 offset-l2 s12">
+            <div className="col m4 l2  s12">
               <a className="grey-text text-lighten-4" href='https://github.com/Rayzev/ResponsividadeMaterialize'>
-              <h5 className="white-text">Codigo: GitHub</h5>
+                <h5 className="white-text mouse">Código disponível no GitHub</h5>
               </a>
+            </div>
+            <div class="col s4 l4 s12">
+              <h5 class="white-text">Links de Contato</h5>
+              <ul>
+                <div className='col s2 m2 l2'>
+                  <li ><a class="grey-text text-lighten-3" href="https://www.instagram.com/rayrayazevedo/" ><FaInstagram className='icon-size'></FaInstagram></a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/rayldoazevedo"><FaFacebook className='icon-size'></FaFacebook></a></li>
+                </div>
+                <div className='col s2 m2 l2'>
+                  <li><a class="grey-text text-lighten-3" href="https://github.com/Rayzev"><FaGithub className='icon-size'></FaGithub></a></li>
+                  <li><a class="grey-text text-lighten-3 mouse" href="https://vercel.com/">Vercel</a></li>
+                </div>
+              </ul>
             </div>
           </div>
         </div>
         <div className="footer-copyright">
           <div className="container">
             © 2022 Copyright Text
-            <a className="grey-text text-lighten-4 right" href="https://vercel.com/">Vercel</a>
           </div>
         </div>
       </footer>
